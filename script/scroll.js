e7a7a031b5fddf934514e5082e6adb42ed0fa23b
YashVarpe05 <yashvarpe2005@gmail.com>
2024-10-28 14:26:53 +0530

    Refactor image sources and dimensions in website

--- script/scroll.js
+++ script/scroll.js
@@ -1,7 +1,7 @@
 var Scrollbar = window.Scrollbar;
 
 Scrollbar.init(document.body, {
-	damping: 0.07, // Smooth but responsive scrolling
+	damping: 0.09, // Smooth but responsive scrolling
 	alwaysShowTracks: false, // Hides the scroll tracks
 	continuousScrolling: true, // Allows smooth scrolling when reaching page boundaries
 });
